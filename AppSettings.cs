149a508a0523e92cb2d203b807d3277150ff3466
Howard Hecht <hhecht@uniteddx.com>
2020-12-03 14:55:43 -0500

    Initial Commit

---
+++ AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HL7toJSON
+{
+    public class AppSettings
+    {
+        public string InputFolder { get; set; }
+        public string OutputFolder { get; set; }
+    }
+}
